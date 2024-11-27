2297bdd82682ef65ca1320b335bf619c4983fd87
A23HugoTF <a23hugotf@a26eql03.sanclemente.local>
2024-11-27 17:37:46 +0100

    Update: replaced contraint layout for linear, Added: ic_home and img of first deign

--- app/src/main/java/com/example/pmdm_uf1_jestify/ui/create/CreateViewModel.kt
+++ app/src/main/java/com/example/pmdm_uf1_jestify/ui/create/CreateViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class CreateViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is to create jokes"
+        value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod, libero nec venenatis tincidunt, lorem nisi posuere lacus, in fermentum eros arcu non nulla. Fusce ut erat id erat volutpat elementum. Suspendisse accumsan nisl ac odio cursus, id fermentum mi tristique. Cras dapibus ligula eget eros tempus venenatis. Integer aliquet felis ut justo sodales, ac volutpat enim viverra. Ut eu nisi augue. Phasellus et erat ac eros dignissim tincidunt."
     }
     val text: LiveData<String> = _text
 }
